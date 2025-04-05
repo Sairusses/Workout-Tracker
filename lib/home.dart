230d0aea0d41ca1cf1d9bc833e55ca0d0d0e1f14
Sairusses <kurtarias123@gmail.com>
2025-04-06 00:59:36 +0800

    added navigation bar

--- lib/home.dart
+++ lib/home.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatefulWidget{
   const Home({super.key});
+  @override
+  State<Home> createState() => HomeState();
+}
+class HomeState extends State<Home>{
+  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        indicatorColor: Colors.blueGrey,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.calendar_month_rounded),
+            icon: Icon(Icons.calendar_month_outlined),
+            label: 'Calendar',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.fitness_center_rounded),
+            icon: Icon(Icons.fitness_center_outlined),
+            label: 'Sets',
+          ),
+        ]
+      ),
+      body: <Widget>[
+        const Center(child: Text('Home')),
+        const Center(child: Text('Calendar')),
+        const Center(child: Text('Sets')),
+      ][currentPageIndex],
     );
   }
-
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: kDebugMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Home(),
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.light(),
+      home: const Home(),
     );
   }
 }
